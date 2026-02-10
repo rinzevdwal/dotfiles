f3f98e329b3a42629e180b20f0956c80ae5d5f57
Rinze van der Wal <rinzevdw@ghs.com>
2026-02-10 10:27:51 +0100

    fix: mutex not released by background process

--- conf.polybar/.config/polybar/launch.sh
+++ conf.polybar/.config/polybar/launch.sh
@@ -1,42 +1,7 @@
 #!/usr/bin/env bash
 
-LOCKFILE="/tmp/$(basename "$0").$UID.lock"
-exec {LOCK_FD}<>"$LOCKFILE"
-if ! flock -n "$LOCK_FD"; then
+# Use flock/mutex to prevent script race conditions (multiple instance running or half closed by script)
+# Use nonblock because there is no need to restart polybar again after a restart.
+# Use close because the lock should not be hold by polybar running as a daemon.
+flock --nonblock --close ~/.config/polybar/lockfile.lock ~/.config/polybar/_launch.sh || \
     echo "Error: Another instance of $(basename "$0") is already running." >&2
-    exit 1
-fi
-
-# Terminate already running bar instances
-killall -q polybar
-
-# Wait until the processes have been shut down
-while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
-
-# Get the list of monitors
-# The output of polybar --list-monitors looks like:
-# HDMI-1: 1920x1080+0+0 (primary)
-# eDP-1: 1920x1080+1920+0
-
-polybar --list-monitors | while read -r line; do
-    # Extract the monitor name (everything before the colon)
-    m=$(echo $line | cut -d":" -f1)
-    
-    # Check if the line contains the word "(primary)"
-    if [[ $line == *"(primary)"* ]]; then
-        echo "--- Launching bars + tray on $m ---"
-        MONITOR=$m polybar --reload bar_menu &
-        MONITOR=$m polybar --reload bar_i3 &
-        MONITOR=$m polybar --reload bar_date &
-        MONITOR=$m polybar --reload bar_stats &
-        MONITOR=$m polybar --reload bar_tray &
-    else
-        echo "--- Launching bars on $m ---"
-        MONITOR=$m polybar --reload bar_menu &
-        MONITOR=$m polybar --reload bar_i3 &
-        MONITOR=$m polybar --reload bar_date &
-        MONITOR=$m polybar --reload bar_stats &
-    fi
-done
-
-echo "Bars launched..."
