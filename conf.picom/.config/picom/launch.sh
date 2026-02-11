a33fd6dd71c584a791f40c9d3e2b6af7ec5ab25f
Rinze van der Wal <rinzevdw@ghs.com>
2026-02-11 11:53:26 +0100

    feat: don't start picom for virtual machines

--- conf.picom/.config/picom/launch.sh
+++ conf.picom/.config/picom/launch.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
 
-# Check if a xrdp session is running
-# if not then start picom else don't start picom
-# picom doesn't work well with remote sessions.
-if [ "$I3_SESSION_TYPE" != "xrdp" ]; then
+# Check if we are in a remote xrdp session
+# AND check if we are running inside a Virtual Machine.
+# Picom is disabled in both cases to ensure performance.
+# Picom doesn't work well with remote sessions and virtual machines.
+
+VIRT_TYPE=$(systemd-detect-virt)
+
+if [ "$I3_SESSION_TYPE" != "xrdp" ] && [ "$VIRT_TYPE" == "none" ]; then
     picom --daemon
 fi
